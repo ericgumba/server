24fa3abf538bc40432ec4d641317c35b9960ce22
okmanl <ericgumba@gmail.com>
2018-01-30 02:55:59 -0800

    fixed typo in prod.js

--- config/prod.js
+++ config/prod.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret: proccess.env.GOOGLE_CLIENT_SECRET,
-    mongoURI: proccess.env.MONGO_URI,
-    cookieKey: proccess.env.COOKIE_KEY
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
 }
