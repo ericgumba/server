0ca56a82bc8f6fd4256132446568eab052045353
okmanl <ericgumba@gmail.com>
2018-01-30 02:49:44 -0800

    finished oauth flow

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+//prod.js -- production keys here!
+
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: proccess.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: proccess.env.MONGO_URI,
+    cookieKey: proccess.env.COOKIE_KEY
+}
--- index.js
+++ index.js
@@ -2,35 +2,35 @@ const express = require('express')
 const mongoose = require('mongoose')
 const CookieSession = require('cookie-session')
 const passport = require('passport')
-    // const keys = require('./config/keys')
-    // require('./models/User')
-    // require('./services/passport')
+const keys = require('./config/keys')
+require('./models/User')
+require('./services/passport')
 
 
-// let days = amountOfDays => amountOfDays * 24 * 60 * 60 * 1000;
+let days = amountOfDays => amountOfDays * 24 * 60 * 60 * 1000;
 
-// mongoose.Promise = global.Promise;
-// mongoose.connect(keys.mongoURI);
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-// console.log(keys.cookieKey);
-// app.use(
-//     CookieSession({
-//         maxAge: 30 * 24 * 60 * 60 * 1000, // how long cookie exist before it expires.
-//         keys: [keys.cookieKey]
-//     })
-// );
+console.log(keys.cookieKey);
+app.use(
+    CookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000, // how long cookie exist before it expires.
+        keys: [keys.cookieKey]
+    })
+);
 
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 app.get('/', (req, res) => {
     res.send('<p> You\'re slowly getting to your goal! =) </p>')
 })
 
-// require('./routes/authRoutes')(app);
+require('./routes/authRoutes')(app);
 
 
 const PORT = process.env.PORT || 5000;
